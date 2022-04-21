821ea1b94177c892a892d9d65a3ab91cef35c821
Sasha Glanville <51757958+sglanvil@users.noreply.github.com>
2022-04-21 12:30:42 -0600

    Create checkHash_e3smv1.sh

---
+++ checkHash_e3smv1.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+case=e3smv1.20TR_CMIP6.ne30_oECv3_ICG.LE-v1.0365.004
+
+# ---------- only doing /atm/hist/ for now...
+src=/global/cscratch1/sd/$USER/archive/$case/atm/hist/
+dest=/global/cfs/cdirs/mp9/archive/e3smv1/$case/atm/hist/
+
+for fullSrc in $src/*; do
+        file=$(basename $fullSrc)
+        echo $file
+        hashSrc=$(md5sum $src/$file | cut -d " " -f1) # get hash of src file
+        if [ -f "$dest"/"$file" ]; then # make sure dest file exists
+                hashDest=$(md5sum $dest/$file | cut -d " " -f1) # then get hash of dest file
+
+                # -------------- FOR TESTING ------------
+                # hashDest=d8059ef1d9066223c9fb0e73039ff664 # uncomment this, if you wish to test
+                # ---------------------------------------
+
+                echo $hashSrc
+                echo $hashDest
+                if [[ "$hashSrc" == "$hashDest" ]]; then
+                        echo "okay to remove source file"
+                        # rm $src/$file # -------------- WHEN READY, UNCOMMENT THIS LINE
+                else
+                        echo "do NOT remove source file"
+                fi
+        fi
+        echo --------------------------
+done
