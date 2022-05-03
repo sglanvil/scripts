6b8b5025592d18b2a16097c64b30d373ee5295c0
Sasha Glanville <51757958+sglanvil@users.noreply.github.com>
2022-05-03 11:21:30 -0600

    Update checkHash_nanr.sh

--- checkHash_nanr.sh
+++ checkHash_nanr.sh
@@ -2,6 +2,7 @@
 
 # This script creates a file listing of all files ready to be deleted.
 # These files exist as duplicates, present on both $src and $dest directories.
+# This script checks all archive files (all components, even non-hist subdirectories).
 # This script uses MD5 hashes, checking size and corruption.
 
 # ------------------------- SETUP --------------------------
@@ -17,28 +18,26 @@
 case=e3smv1.20TR_CMIP6.ne30_oECv3_ICG.LE-v1.0210.001
 src=/global/cscratch1/sd/nanr/archive/$case/
 dest=/global/cfs/cdirs/mp9/archive/e3smv1/$case/
+# do this if you want to empty the files:
+echo > removeList_nanr
+echo > keepList_nanr
 # ------------------- USER SPECIFY ABOVE -------------------
 
 for fullSrc in $(find $src -name '*' -type f); do
         file=$(sed -e 's/.*\///' <<< $fullSrc)
-        middlePath=$(sed -e "s~$src~~" <<< $fullSrc | sed -e "s/$file//") # Using tildas because $src variable has forward slashes
+        middlePath=$(sed -e "s~$src~~" <<< $fullSrc | sed -e "s/$file//")
         echo $fullSrc
         hashSrc=$(md5sum $src/$middlePath/$file | cut -d " " -f1) # get hash of src file
         if [ -f "$dest"/"$middlePath"/"$file" ]; then # make sure dest file exists
                 hashDest=$(md5sum $dest/$middlePath/$file | cut -d " " -f1) # then get hash of dest file
-                # echo $hashSrc
-                # echo $hashDest
                 if [[ "$hashSrc" == "$hashDest" ]]; then
-                        # echo "okay to remove source file"
-                        ls $src/$middlePath/$file >> removeList_nanr
+                        ls $src/$middlePath/$file >> removeList_nanr # OKAY to remove src file
                 else
-                        ls $src/$middlePath/$file >> keepList_nanr
-                        # echo "do NOT remove source file (dest file is corrupt)"
+                        ls $src/$middlePath/$file >> keepList_nanr # do NOT remove src file (dest file is corrupt)
                 fi
         else
-                ls $src/$middlePath/$file >> keepList_nanr
-                # echo "do NOT remove source file (dest file does not exist)"
+                ls $src/$middlePath/$file >> keepList_nanr # do NOT remove src file (dest file does not exist)
         fi
-        echo --------------------------
 done
+
 
