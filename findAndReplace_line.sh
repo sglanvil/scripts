6de6ceceab05b06e8cb01dbca213f71ed41cc40f
Sasha Glanville <51757958+sglanvil@users.noreply.github.com>
2022-05-19 13:47:22 -0600

    Create findAndReplace_line.sh

---
+++ findAndReplace_line.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+case=b.e21.BHISTcmip6.f09_g17.CESM2-SF-xAER
+
+for imember in {004..010}; do
+        newCase=$case.$imember
+        cd /glade/scratch/sglanvil/post-proc/$newCase/postprocess/
+        pwd
+        sed -i "s/CASE=b.e21.BHISTcmip6.f09_g17.CESM2-SF-xAER.004/CASE=$newCase/g" timeseries
+        sed -i "s/nanr/sglanvil/g" timeseries
+done
+
