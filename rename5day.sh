5412b7f3810347a9073c89088594872f5e7f8f62
Sasha Glanville <51757958+sglanvil@users.noreply.github.com>
2022-05-19 13:49:36 -0600

    Create rename5day.sh

---
+++ rename5day.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# run as "cesmsf"
+whoami
+
+archiveDir=/glade/scratch/cesmsf/archive/
+caseName=b.e21.BHISTcmip6.f09_g17.CESM2-SF-xAER
+
+for imember in {004..010}; do
+        echo $archiveDir/$caseName.$imember/ocn/hist/
+        cd $archiveDir/$caseName.$imember/ocn/hist/
+        rename "5day" "nday5" *.5day.*
+done
+
