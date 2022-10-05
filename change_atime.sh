737165e74bb73745df21bc26fd332c4ce947d849
Sasha Glanville <51757958+sglanvil@users.noreply.github.com>
2022-10-05 13:57:15 -0600

    Update change_atime.sh

--- change_atime.sh
+++ change_atime.sh
@@ -4,7 +4,8 @@
 
 # change the access time without using "touch" (loophole, I guess)
   
-dir=/global/cscratch1/sd/sglanvil/archive/e3smv1.20TR_CMIP6.ne30_oECv3_ICG.LE-v1.0365.004/
+#dir=/global/cscratch1/sd/sglanvil/archive/e3smv1.20TR_CMIP6.ne30_oECv3_ICG.LE-v1.0365.004/
+dir=/global/cscratch1/sd/sglanvil/E3SMv1_simulations/
 
 for fil in $(find $dir -type f); do
         echo $fil
